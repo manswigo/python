5579e969ec211c10f736c9150b5b55d48221e577
manswigo <manswigo@gmail.com>
2025-10-15 11:25:48 +0200

    kuk

--- python_matte/test.py
+++ python_matte/test.py
@@ -1,7 +1,7 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-def sin_taylor(x):
+def sin_taylor(x, i):
     tecken = 1
     while x >= 2*math.pi:
        x -= 2*math.pi
@@ -13,11 +13,11 @@ def sin_taylor(x):
     if x > math.pi/2:
         x = math.pi-x
     sin_sum = 0
-    for n in range(201):
+    for n in range(i):
         sin_sum += ((((-1)**n)/(math.factorial(2*n+1)))*(x**(2*n+1)))  
     return(sin_sum*tecken)
 
-def cos_taylor(x):
+def cos_taylor(x, i):
     tecken = 1
     while x >= 2*math.pi:
        x -= 2*math.pi
@@ -29,7 +29,7 @@ def cos_taylor(x):
     
 
     cos_sum = 0
-    for n in range(201):
+    for n in range(i):
         cos_sum += (((-1)**n)/(math.factorial(2*n)))*(x**(2*n))
     return(tecken*cos_sum)
 
@@ -37,17 +37,31 @@ def cos_taylor(x):
 x = np.linspace(-10, 10, 1000)
 y = []
 y1 = []
-y2 = 0
+n = 260
 for i in x:
-    y.append(abs(cos_taylor(i)-np.cos(i)))
-    y1.append(abs(sin_taylor(i)-np.sin(i)))
+    y.append(abs(cos_taylor(i, n) - np.cos(i)))
+    y1.append(abs(sin_taylor(i, n) - np.sin(i)))
 fig, ax = plt.subplots()
+ax.set_xlim(-10, 10)
 ax.set_xticks([-3*math.pi, -2*math.pi, -1*math.pi, 0,  math.pi, 2*math.pi, 3*math.pi])
 ax.plot(x, y)
 ax.plot(x, y1)
 plt.hlines(10**-15, -10, 10)
 
 
+'''for i in range(0, 50, 10):
+    
+    largest = 0
+    for n in x:
+        y.append(cos_taylor(n, i))
+        y1.append(sin_taylor(n, i))
+    for k in range(len(y)):
+        if y[k] - math.cos(k) > largest:
+            largest = k - np.cos(x)
+    if largest >= 10**-15:
+        print('bäst n=', i)
+        break'''
+    
 plt.show()
 
 
